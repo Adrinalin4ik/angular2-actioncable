1e92c201b5f0019af3c08a9501c2c95aa75d6005
Alex Panfilkin <soild@mail.ru>
2018-03-21 14:08:11 +0300

    Update cable.ts

--- src/cable.ts
+++ src/cable.ts
@@ -35,15 +35,31 @@ export class Channel {
   onclose: Observable<any>;
   onerror: Observable<any>;
   onconnected: Observable<any>;
+  
   constructor(public cable: Cable, public name: string, public params = {}) {
     console.warn("NEW")
     const channelParams = Object.assign({}, params, {channel: name});
     this.baseChannel = this.cable.baseCable.subscriptions.create(channelParams, {
-      received: (data: any) => { this.messages = Observable.create(observer => {observer.next(data}) },
-      connected: (data: any) => { this.onconnected = Observable.create(observer => {observer.next(data}) },
-      open: (data: any) => { this.onopen = Observable.create(observer => {observer.next(data}) },
-      close: (data: any) => { this.onclose = Observable.create(observer => {observer.next(data}) },
-      error: (data: any) => { this.onerror = Observable.create(observer => {observer.next(data}) }
+      received: (data: any) => { 
+        this.messages = Observable.create(observer => observer.next(data));
+        return () => this.unsubscribe();
+      },
+      connected: (data: any) => { 
+        this.onconnected = Observable.create(observer => observer.next(data));
+        return () => this.unsubscribe();
+      },
+      open: (data: any) => { 
+        this.onopen = Observable.create(observer => observer.next(data));
+        return () => this.unsubscribe();
+      },
+      close: (data: any) => { 
+        this.onclose = Observable.create(observer => observer.next(data));
+        return () => this.unsubscribe();
+      },
+      error: (data: any) => { 
+        this.onerror = Observable.create(observer => observer.next(data));
+        return () => this.unsubscribe();
+      }
     });
     //return () => this.unsubscribe();
   }
