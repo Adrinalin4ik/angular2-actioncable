582cc7e6e7e32cd3fc8258e962db285c8ef32fc2
Alex Panfilkin <soild@mail.ru>
2018-03-21 14:02:27 +0300

    Update cable.ts

--- src/cable.ts
+++ src/cable.ts
@@ -29,23 +29,23 @@ export class Channel {
    * Once a channel subscription is created, the messages Observable will emit any messages the channel receives.
    * For easy clean-up, when this Observable is completed the ActionCable channel will also be closed.
    */
-  messages: Observable<any> = new Observable();
-  baseChannel: any = new Observable();
-  onopen: Observable<any> = new Observable();
-  onclose: Observable<any> = new Observable();
-  onerror: Observable<any> = new Observable();
-  onconnected: Observable<any> = new Observable();
+  messages: Observable<any>
+  baseChannel: any;
+  onopen: Observable<any>;
+  onclose: Observable<any>;
+  onerror: Observable<any>;
+  onconnected: Observable<any>;
   constructor(public cable: Cable, public name: string, public params = {}) {
     console.warn("NEW")
     const channelParams = Object.assign({}, params, {channel: name});
     this.baseChannel = this.cable.baseCable.subscriptions.create(channelParams, {
-      received: (data: any) => this.messages.next(data),
-      connected: (data: any) => this.onconnected.next(data),
-      open: (data: any) => this.onopen.next(data),
-      close: (data: any) => this.onclose.next(data),
-      error: (data: any) => this.onerror.next(data)
+      received: (data: any) => { this.messages = Observable.create(observer => {observer.next(data}) },
+      connected: (data: any) => { this.onconnected = Observable.create(observer => {observer.next(data}) },
+      open: (data: any) => { this.onopen = Observable.create(observer => {observer.next(data}) },
+      close: (data: any) => { this.onclose = Observable.create(observer => {observer.next(data}) },
+      error: (data: any) => { this.onerror = Observable.create(observer => {observer.next(data}) }
     });
-    return () => this.unsubscribe();
+    //return () => this.unsubscribe();
   }
 
   /**
